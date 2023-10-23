913eea74f9cff4de9748456b20b4b05cac552acb
Fabián E. García <fabianegl79@gmail.com>
2023-10-23 19:28:22 -0300

    movimiento vertical, horizontal con animacion IDLE y WALk

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 2f;
+
+    private Rigidbody2D _rigidbody;
+    private Animator _animator;
+
+    private Vector2 _movement;
+
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Movimiento
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+        _movement = new Vector2(horizontalInput, verticalInput);
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontalVelocity = _movement.normalized.x * speed;
+        float verticalVelocity = _movement.normalized.y * speed;
+        _rigidbody.velocity = new Vector2(horizontalVelocity, verticalVelocity);
+    }
+
+    private void LateUpdate()
+    {
+        _animator.SetBool("Idle", _movement == Vector2.zero);
+        _animator.SetBool("Walk", _movement != Vector2.zero);
+    }
+}
