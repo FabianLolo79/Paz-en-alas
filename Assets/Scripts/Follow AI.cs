46edacba33300bd82cdd414d00757d84108eb656
Fabián E. García <fabianegl79@gmail.com>
2023-10-30 09:09:28 -0300

    arreglos varios

---
+++ Assets/Scripts/Follow AI.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Tilemaps;
+using UnityEngine;
+
+public class FollowAI : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    [SerializeField] private Transform player;
+
+    private bool _isFacingLeft = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.position,speed * Time.deltaTime );
+
+        bool _isPlayerLeft = transform.position.x > player.transform.position.x;
+        Flip(_isPlayerLeft);
+    }
+
+    private void Flip(bool isPlayerLeft)
+    {
+        if ((_isFacingLeft && !isPlayerLeft) || (!_isFacingLeft && isPlayerLeft))
+        {
+            _isFacingLeft = !_isFacingLeft;
+            Vector3 scale = transform.localScale;
+            scale.x *= -1;
+            transform.localScale = scale;
+        }
+    }
+
+}
